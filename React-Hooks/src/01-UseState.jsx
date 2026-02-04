5e898af37571279ea7c589653d1bd6c138797c19
Nasir-afridi <nasirwc05@gmail.com>
2026-02-04 22:48:44 +0500

    added the decrement for the counter

--- React-Hooks/src/01-UseState.jsx
+++ React-Hooks/src/01-UseState.jsx
@@ -1,14 +1,21 @@
 import React, { useState } from 'react'
 
 const UseState = () => {
-    const [count, setCount] = useState(0)
-    function counter () {
+    const [count, setCount] = useState(0);
+
+    function increment () {
         setCount(count + 1)
+    }
+    function decrement () {
+        setCount(count - 1)
     }
   return (
     <>
     <h1>You Click {count} Times </h1>
-    <button onClick={counter}>Increment</button>
+    <button onClick={increment}>Increment</button>
+    <br />
+    <br />
+    <button onClick={decrement}>Decrement</button>
     </>
   )
 }
