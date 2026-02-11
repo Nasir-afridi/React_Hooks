e91c42adbd2f7a3c7838a72e1fd7b50643125554
Nasir-afridi <nasirwc05@gmail.com>
2026-02-11 15:50:37 +0500

    added the short summary for useState hook

--- React-Hooks/src/Hooks/01-UseState.jsx
+++ React-Hooks/src/Hooks/01-UseState.jsx
@@ -1,32 +1,52 @@
-import React, { useState } from 'react'
+/*
+State is a special variable in React that stores data for a component.
+
+It cannot be updated directly; you must use the provided setter function (like setState or setCount).
+
+State updates are asynchronous — React batches updates for performance.
+
+Changing state triggers React to re-render the component, so the updated data appears on the UI.
+
+useState must be imported from React when using function components:
+import { useState } from 'react';
+
+Do not confuse state with normal variables: updating a normal variable does not trigger re-render.
+*/
+
+
+import React, { useState } from "react";
 
 const UseState = () => {
-    const [count, setCount] = useState(0);
-    const [text, setText] = useState('');
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
 
-    function increment () {
-        setCount(count + 1)
-    }
-    function decrement () {
-        setCount(count - 1)
-    }
-    function reset() {
-      setCount(0)
-    }
+  function increment() {
+    setCount(count + 1);
+  }
+  function decrement() {
+    setCount(count - 1);
+  }
+  function reset() {
+    setCount(0);
+  }
   return (
     <>
-    <h1>You Click {count} Times </h1>
-    <button onClick={increment}>+1</button>
-    <br />
-    <br />
-    <button onClick={decrement}>-1</button>
-    <br />
-    <br />
-    <button onClick={reset}>Reset</button>
-    <h1>You Typed : {text}</h1>
-    <input value={text} type="text" onChange={(e) => setText(e.target.value)}/>
+      <h1>You Click {count} Times </h1>
+      <button onClick={increment}>+1</button>
+      <br />
+      <br />
+      <button onClick={decrement}>-1</button>
+      <br />
+      <br />
+      <button onClick={reset}>Reset</button>
+      <h1>You Typed : {text}</h1>
+      <input
+        value={text}
+        type="text"
+        onChange={(e) => setText(e.target.value)}
+      />
     </>
-  )
-}
+  );
+};
 
 export default UseState;
