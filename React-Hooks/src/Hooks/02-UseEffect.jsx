cf574b31c151ce41bab4a284595c4be888265372
Nasir-afridi <nasirwc05@gmail.com>
2026-02-18 23:32:48 +0500

    added the second state for better understanding of useEffect hook

--- React-Hooks/src/Hooks/02-UseEffect.jsx
+++ React-Hooks/src/Hooks/02-UseEffect.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const UseEffect = () => {
   const [count, setCount] = useState(0);
+  const [data, setData] = useState(0);
   // button ko click krny pp function hamesha call hoga q k component rerender horha hai isiliye function be pher sy call horha hai isko side effect bolty hain or ye useeffect sy handle hoga.
   function callOnce() {
     console.log("CallOnce");
@@ -9,8 +10,12 @@ const UseEffect = () => {
   callOnce();
   return (
     <>
-      <h1>Counting is : {count}</h1>
+      <h1>
+        Counting is : {count} {data}
+      </h1>
+
       <button onClick={() => setCount(count + 1)}>Counter</button>
+      <button onClick={() => setData(data + 1)}>Data</button>
     </>
   );
 };
