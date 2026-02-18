3661f4ec26951d6d7084b6c85f82b54ce82903d2
Nasir-afridi <nasirwc05@gmail.com>
2026-02-19 00:46:17 +0500

    received the props and show it in heading

---
+++ React-Hooks/src/Props/Data.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Data = ({ Class, Name }) => {
+  return (
+    <>
+      <h1>
+        Hi my Name is {Name} - My Class is {Class}
+      </h1>
+    </>
+  );
+};
+
+export default Data;
