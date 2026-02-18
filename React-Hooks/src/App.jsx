2371ce0e69d8d85b94413ba6956104fdc6f1bfdc
Nasir-afridi <nasirwc05@gmail.com>
2026-02-19 00:27:32 +0500

    added the counter component send the props to it of state

--- React-Hooks/src/App.jsx
+++ React-Hooks/src/App.jsx
@@ -1,11 +1,19 @@
 // import UseState from "./Hooks/01-UseState";
-import UseEffect from "./Hooks/02-UseEffect";
+// import UseEffect from "./Hooks/02-UseEffect";
+import { useState } from "react";
+import Counter from "./Props/Counter.jsx";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(0);
   return (
     <>
       {/* <UseState /> */}
-      <UseEffect />
+      {/* <UseEffect /> */}
+
+      <Counter countIs={count} myData={data} />
+      <button onClick={() => setCount(count + 1)}>Count</button>
+      <button onClick={() => setData(data + 1)}>Data</button>
     </>
   );
 }
