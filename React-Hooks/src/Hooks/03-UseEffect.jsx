bed6b1440a30ba8a8a84bf353cbc2e934ecdb18b
Nasir-afridi <nasirwc05@gmail.com>
2026-02-21 23:42:34 +0500

    Working of Life cycle in useEffect comment added

---
+++ React-Hooks/src/Hooks/03-UseEffect.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+/*
+    Life Cycle in UseEffect
+
+    useEffect(() => {
+    This code will run on mount
+    }, []);
+
+    useEffect(() => {
+    This code will run on update
+    }, [state or props]);
+
+    useEffect(() => {
+    return () => {
+    This code will run on unmount
+    };
+    }, []);
+
+    useEffect(() => {
+    This code will run on every life cycle event
+    });
+*/
+
+const UseEffect = () => {
+  return (
+    <>
+      <h1>03-UseEffect</h1>
+    </>
+  );
+};
+
+export default UseEffect;
